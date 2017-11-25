5460e846449b62031d555caa84b411f27e995d0b
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2017-11-25 12:31:49 +0100

    Added hook on file_shared event, added saving user data in the database and fetching file data

--- src/commands/FileSharedCommand.js
+++ src/commands/FileSharedCommand.js
@@ -1,13 +1,66 @@
 'use strict';
 
 export default class {
-  constructor (slack) {
-    slack.on('event', (payload, bot) => {
+  constructor(slack, usersService) {
+    this.usersService = usersService;
+    slack.on('event', async (payload, client) => {
+      this.client = client;
+
+      console.log('Received event', payload.event.type);
       if (payload.event.type !== 'file_shared') {
         return;
       }
 
-      console.log(payload);
+      console.log('Processing payload', payload);
+      await this.ensureUserExists(payload.event.user_id);
+      const file = await this.getFile(payload.event.file_id);
     });
+  }
+
+  async ensureUserExists(userId) {
+    console.log('Loading user', userId);
+    const user = await this.usersService.get(userId);
+    if (!user) {
+      console.log('User does not exist');
+
+      let user;
+      try {
+        user = await this.client.send('users.info', {
+          token: process.env.OAUTH_ACCESS_TOKEN,
+          user: userId
+        });
+      } catch (e) {
+        console.error('Unable to fetch user', e);
+        throw e;
+      }
+
+      console.log('Loaded user data', user);
+
+      const userData = {
+        id: userId,
+        username: user.user.name,
+        fullName: user.user.real_name
+      };
+
+      console.log('Saving user data', userData);
+      return this.usersService.put(userData);
+    }
+  }
+
+  async getFile(fileId) {
+    console.log('Loading file', fileId);
+    let file;
+    try {
+      file = await this.client.send('files.info', {
+        token: process.env.OAUTH_ACCESS_TOKEN,
+        file: fileId
+      });
+      console.log('Loaded file data', file);
+    } catch (e) {
+      console.error('Unable to fetch file', e);
+      throw e;
+    }
+
+    return file;
   }
 }
--- src/slack.js
+++ src/slack.js
@@ -1,9 +1,14 @@
 'use strict';
 
+import AWS from 'aws-sdk';
+import DynamoDBService from './services/DynamoDBService';
 import slack from "serverless-slack";
+
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const usersService = new DynamoDBService(dynamoDbClient, process.env.USER_SETTINGS_TABLE_NAME);
 
 import FileSharedCommand from './commands/FileSharedCommand';
 
-new FileSharedCommand(slack);
+new FileSharedCommand(slack, usersService);
 
 export const handler = slack.handler.bind(slack);
