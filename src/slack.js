e8997be89473e302cbbc6a95c179e10ff358057d
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2017-12-05 20:11:48 +0100

    Moved import statements to the top of the file

--- src/slack.js
+++ src/slack.js
@@ -4,12 +4,12 @@ import AWS from 'aws-sdk';
 import DynamoDBService from './services/DynamoDBService';
 import slack from "serverless-slack";
 
+import FileSharedCommand from './commands/FileSharedCommand';
+import FileUnsharedCommand from './commands/FileUnsharedCommand';
+
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 const usersService = new DynamoDBService(dynamoDbClient, process.env.USERS_TABLE_NAME);
 const filesService = new DynamoDBService(dynamoDbClient, process.env.FILES_TABLE_NAME);
-
-import FileSharedCommand from './commands/FileSharedCommand';
-import FileUnsharedCommand from './commands/FileUnsharedCommand';
 
 new FileSharedCommand(slack, usersService, filesService);
 new FileUnsharedCommand(slack, filesService);
